f3622d15f189decfaafdbbba2ba6391fea154776
SATYAM SUMAN <146647625+SATYAMSUMAN24@users.noreply.github.com>
2024-02-20 22:06:13 +0530

    Add files via upload

---
+++ pattern/invertedhalfpyramid.java
@@ -0,0 +1,21 @@
+// inverted & rotated half pyramid
+import  java.util.*;
+public class invertedhalfpyramid{
+    public static void inverted_half_pyramid(int n){
+        // outer
+        for(int i = 1; i<=n; i++){
+            // spaces
+            for(int j=1; i<=n-i; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        inverted_half_pyramid(4);
+    }
+}
